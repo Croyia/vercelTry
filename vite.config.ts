8b31a6618bcd071526ad09fcd28fdc439d74ce1f
Dey <andreilee0822@gmail.com>
2025-02-08 21:56:58 +0800

    Vercel-003: Edited again again

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vercelTry',
+  base: '/', 
   resolve: {
     alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`
+      '@': path.resolve(__dirname, './src')
     }
   },
+  build: {
+    outDir: 'dist'
+  }
 })
